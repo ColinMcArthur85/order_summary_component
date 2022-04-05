bd9460f7300fdff93f04a4891afa34c8388f2caf
ColinMcArthur85 <85662396+ColinMcArthur85@users.noreply.github.com>
2022-04-05 11:13:56 -0700

    Update sytlesheets/config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      "pale-blue": "hsl(225, 100%, 94%)",
+      "bright-blue": "hsl(245, 75%, 52%)",
+      "very-pale-blue": "hsl(225, 100%, 98%)",
+      "desaturated-blue": "hsl(224, 23%, 55%)",
+      "dark-blue": "hsl(223, 47%, 23%)",
+    },
   },
+  extend: {},
   plugins: [],
 };
